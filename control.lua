68638912eb982a609fa892766cee6efafa037a3d
tno1 <nnordendorf@gmail.com>
2024-11-03 18:00:00 +0100

    Added GUI Keybind/Shortcut working
    I have added a way to hide the GUI using keybinding or the menu shortcut
    Default keybinding: CTRL+M
    
    Changed the EXP Label to be "ENTITY NAME EXP"

--- control.lua
+++ control.lua
@@ -1,18 +1,37 @@
 require("util")
+require("mod-gui")
 local filter = { { filter = "type", type = "ammo-turret" },
 	{ filter = "type", type = "assembling-machine" },
 	{ filter = "type", type = "furnace" },
 	{ filter = "type", type = "lab" },
-	{ filter = "type", type = "mining-drill" } }
---local base_time = settings.startup["base_time_to_level_up"].value
-local base_time = 2
+	{ filter = "type", type = "mining-drill" },
+	{ filter = "type", type = "boiler" },
+	{ filter = "type", type = "generator" },
+	{ filter = "type", type = "solar-panel" },
+	{ filter = "type", type = "accumulator" },
+	{ filter = "type", type = "reactor" },
+	{ filter = "type", type = "beacon" }
+}
+local base_time = settings.startup["base_time_to_level_up"].value
 local multiplier = settings.startup["multiplier_time_to_level_up"].value
 local is_update_module = settings.startup["randomly_upgrade_inside_module"].value
 local base_time_module = settings.startup["time_to_randomly_levelup_inside_module"].value
 local respect_technology = settings.startup["respect_technology"].value
 local quality_tech = {}
+local active_gui = false
+
 script.on_init(function()
 	get_built_machine()
+	
+    for _, player in pairs(game.players) do
+        player.set_shortcut_toggled('toggle-machine-exp-gui', active_gui)
+    end
+end)
+
+script.on_event(defines.events.on_player_created, function(event)
+    local player = game.players[event.player_index]
+    -- Set the toggle state of the shortcut for the newly created player
+    player.set_shortcut_toggled('toggle-machine-exp-gui', active_gui)
 end)
 
 script.on_configuration_changed(function()
@@ -22,6 +41,11 @@ script.on_load(function()
 	quality_tech = check_quality_unlock_tech()
 end)
 script.on_nth_tick(60, function(event)
+	
+    for _, player in pairs(game.players) do
+        player.set_shortcut_toggled('toggle-machine-exp-gui', active_gui)
+    end
+
 	local upgrade_machine_list = {}
 	local upgrade_module_list = {}
 	for _, ent in pairs(storage.built_machine) do
@@ -34,7 +58,7 @@ script.on_nth_tick(60, function(event)
 			if not next_tech then break end
 			if not check_quality_unlock(next_tech) and respect_technology then break end
 			if ent.level_time < base_time * multiplier ^ next_tech.level then
-				if ent.entity.status == defines.entity_status.working then
+				if (ent.entity.status == defines.entity_status.working or ent.entity.status == defines.entity_status.fully_charged )and ent.level_time < 9999999 then
 					ent.level_time = ent.level_time + 1
 				end
 			else
@@ -68,7 +92,9 @@ end
 
 function check_quality_unlock(tech)
 	local name = tech.name
+	if name == nil then return false end
 	local tech_name = quality_tech[name]
+	if tech_name == nil then return false end
 	return game.forces["player"].technologies[tech_name].researched
 end
 
@@ -84,6 +110,29 @@ function upgrade_machines(ent)
 	if ent.entity.type == "assembling-machine" then
 		if ent.entity.get_recipe() then
 			new_eneity.set_recipe(ent.entity.get_recipe())
+		end
+	end
+
+	new_eneity.mirroring = ent.entity.mirroring
+	new_eneity.orientation = ent.entity.orientation
+	if ent.entity.temperature then
+		new_eneity.temperature = ent.entity.temperature
+	end
+	if ent.entity.energy then
+		new_eneity.energy = ent.entity.energy
+	end
+	local origin_wire = ent.entity.get_wire_connector(defines.wire_connector_id.circuit_green, false)
+	if origin_wire then
+		local wire = new_eneity.get_wire_connector(defines.wire_connector_id.circuit_green, true)
+		for _, v in pairs(origin_wire.real_connections) do
+			wire.connect_to(v.target)
+		end
+	end
+	origin_wire = ent.entity.get_wire_connector(defines.wire_connector_id.circuit_red, false)
+	if origin_wire then
+		local wire = new_eneity.get_wire_connector(defines.wire_connector_id.circuit_red, true)
+		for _, v in pairs(origin_wire.real_connections) do
+			wire.connect_to(v.target)
 		end
 	end
 	if ent.entity.get_module_inventory() then
@@ -105,9 +154,7 @@ function upgrade_machines(ent)
 	if ent.entity.get_burnt_result_inventory() then
 		replace_inventory(new_eneity.get_burnt_result_inventory(), ent.entity.get_burnt_result_inventory())
 	end
-	new_eneity.mirroring = ent.entity.mirroring
-	new_eneity.direction = ent.entity.direction
-	new_eneity.orientation = ent.entity.orientation
+
 	ent.entity.destroy()
 	storage.built_machine[ent.unit_number] = nil
 	Add_storage(new_eneity)
@@ -191,6 +238,75 @@ function Add_storage(v)
 	}
 end
 
+function is_include(value, tab)
+	for k, v in ipairs(tab) do
+		if v == value then
+			return true
+		end
+	end
+	return false
+end
+
+function On_select_changed(event)
+	if not active_gui then return end
+	if event.last_entity then
+		local player = game.players[event.player_index]
+		local gui_container = player.gui.left
+		local existing_gui = gui_container["machine-exp"]
+
+		-- Check if "quality-module" technology is researched
+		local has_technology = player.force.technologies["quality-module"].researched
+
+		-- Check if there is a last entity and if it matches the filter
+		local matches_filter = false
+		if event.last_entity then
+			for _, condition in ipairs(filter) do
+				if event.last_entity.type == condition.type then
+					matches_filter = true
+					break
+				end
+			end
+		end
+
+		-- If technology is researched and entity matches filter, show or update the GUI
+		local ent_name = event.last_entity.name  -- Store the raw entity name
+		if not existing_gui then
+			
+			existing_gui = gui_container.add({
+				type = "frame",
+				name = "machine-exp",
+				caption = ent_name .. " EXP", -- Getting the localized name
+				direction = "vertical"
+			})
+			storage.gui = existing_gui.add({ type = "label", name = "exp-num", caption = "none" })
+		else
+			existing_gui.caption = ent_name .. " EXP"
+		end
+
+		-- Update the GUI with EXP information if available
+		if storage.built_machine[event.last_entity.unit_number] then
+			storage.gui.visible = true  -- Show the GUI when there is no data
+			storage.gui.caption = (storage.built_machine[event.last_entity.unit_number].level_time or "0")
+		else
+			storage.gui.visible = false  -- Hide the GUI when there is no data
+		end
+	end
+end
+
+function toggle_machine_exp_gui(player)
+    local existing_gui = player.gui.left["machine-exp"]
+    -- Toggle the shortcut
+    player.set_shortcut_toggled('toggle-machine-exp-gui', not active_gui)
+
+    if existing_gui then
+        -- If GUI exists, destroy it
+        existing_gui.destroy()
+		active_gui = false
+    else
+		active_gui = true
+    end
+end
+
 script.on_event(
 	defines.events.on_player_mined_entity,
 	On_mined_entity,
@@ -213,3 +329,20 @@ script.on_event(
 	defines.events.on_built_entity,
 	On_built_entity,
 	filter)
+script.on_event(
+	defines.events.on_selected_entity_changed,
+	On_select_changed
+)
+
+script.on_event("toggle-machine-exp-key", function(event)
+    local player = game.players[event.player_index]
+    toggle_machine_exp_gui(player)
+end)
+
+script.on_event(defines.events.on_lua_shortcut, function(event)
+	if event.prototype_name == 'toggle-machine-exp-gui' then
+		local player = game.players[event.player_index]
+    	toggle_machine_exp_gui(player)
+
+	end
+end)
