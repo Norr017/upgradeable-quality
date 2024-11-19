6a8d264e2f7bcf3969a7f88dd4ffe448147a7cbc
JNjiaonang <101388491+JNjiaonang@users.noreply.github.com>
2024-11-19 12:55:16 +0800

    Update data.lua
    Change the referenced alt-mode-x32.png to alt-mode-x24.png because in the beta version, the official removed alt-mode-x32.png

--- data.lua
+++ data.lua
@@ -7,7 +7,7 @@ data:extend({
         action = "lua",  -- This triggers a Lua event
         localised_name = {"shortcut.toggle-machine-exp-gui"},  -- Ensure this key exists in your locale
         associated_control_input = "toggle-machine-exp-key",  -- Connects to the keybinding
-        icon = "__base__/graphics/icons/shortcut-toolbar/mip/alt-mode-x32.png",  -- Use a base game icon path
+        icon = "__base__/graphics/icons/shortcut-toolbar/mip/alt-mode-x24.png",  -- Use a base game icon path
         icon_size = 32,  -- Size of the icon
         small_icon = "__base__/graphics/icons/shortcut-toolbar/mip/alt-mode-x24.png",  -- Small icon path
         small_icon_size = 24,  -- Size of the small icon
